99cc2b22e80853f31412700d7f6ab3f3b22f0c77
gabimartini <49984490+gabimartini@users.noreply.github.com>
2019-10-13 14:14:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require ('express');
 const consign = require ('consign');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 let app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 consign().include('routes').include('uteis').into(app);
 
-app.listen(4000, '127.0.0.1', ()=>{
+app.listen(process.env.PORT || 4000, '127.0.0.1', ()=>{
 
     console.log('servidor rodando!')
 })
